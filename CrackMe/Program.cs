61bc13b1eca9bbb77c9927a9b9f6b248caea9a35
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2020-05-25 01:39:20 +0200

    Add files via upload

---
+++ CrackMe/Program.cs
@@ -0,0 +1,33 @@
+﻿using Console = Colorful.Console;
+using System.Threading;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace CrackMe
+{
+    class Program
+    {
+
+
+        public static void JustBecauseIWant(string text, int time)
+        {
+            Console.WriteLine(text);Thread.Sleep(time);
+        }
+        static void Main(string[] args)
+        {
+            Console.Title = "CrackMe";
+            List<char> ask = new List<char>() { 'W', 'h', 'a', 't', ' ', 'i', 's', ' ', 't', 'h', 'e', ' ', 'p', 'a', 's', 's', 'w', 'o', 'r', 'd', '?',' '};
+            JustBecauseIWant("Yeah a basic crack me", 500);
+            JustBecauseIWant("And now?\n", 750);
+            Console.WriteWithGradient(ask, Color.Blue, Color.Red, 14);
+            if (Console.ReadLine() == "fuck")
+            {
+                Console.WriteLine("You're a good reverser! :)", Color.Green);
+            } else
+            {
+                Console.WriteLine("Bad password son! :(", Color.Red);
+            }
+            Console.ReadLine();
+        }
+    }
+}
